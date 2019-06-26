01d1ed45234ca75434b83511fc35ccf6e886bde4
Noah Zemlin <noah.zemlin@ou.edu>
2019-06-26 12:28:52 -0500

    Correctly working regex

--- src/controllers/praise.ts
+++ src/controllers/praise.ts
@@ -2,17 +2,15 @@ import axios from 'axios';
 import config from '../config';
 
 export default class PraiseController {
-    public escapeRE: RegExp = /<(?<id>.*)\|(?<name>.*)>/g;
+    public escapeRE: RegExp = /<(.*)\|(.*)>/g;
 
     constructor() {
         // Connect to Redis
     }
 
     public parsePraise(body: any) {
-        const groups = body.text.match(this.escapeRE).groups;
-
         const data = {
-            text: body.user_name + ' has praised ' + groups.name + ' w/ id ' + groups.id,
+            text: body.user_name + ' has praised ' + this.escapeRE.exec(body.text)[2], // (Combined, ID, Name)
         };
 
         const cfg = {
